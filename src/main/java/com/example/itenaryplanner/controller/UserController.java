6bf8998a01b9b6a3b7359161746b10a4226a86e4
MohmadFaizan <faizakodrawala@gmail.com>
2022-06-05 18:01:34 +0530

    cors issue fix

--- src/main/java/com/example/itenaryplanner/controller/UserController.java
+++ src/main/java/com/example/itenaryplanner/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -22,6 +23,7 @@ import com.example.itenaryplanner.security.ApplicationUser;
 import com.example.itenaryplanner.security.CustomDetailsService;
 import com.example.itenaryplanner.service.UserService;
 
+@CrossOrigin(allowedHeaders = "*")
 @RestController
 public class UserController {
 
--- src/main/java/com/example/itenaryplanner/security/SecurityConfig.java
+++ src/main/java/com/example/itenaryplanner/security/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.itenaryplanner.security;
 
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -46,9 +41,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.cors();
-        http.csrf().disable()
-                .authorizeRequests()
+    	http.csrf().disable()
+    		.cors()
+    		.and()
+    		.authorizeRequests()
                 	.antMatchers(HttpMethod.POST, "/authenticate", "/signup")
                 .permitAll().anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -76,15 +72,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	provider.setUserDetailsService(customDetailsService);
     	
     	return provider;
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
